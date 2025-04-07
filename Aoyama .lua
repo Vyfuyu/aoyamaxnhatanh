ecf28ee3f3fe3ab5ff2fcea5930cd139d76182fb
Vyfuyu <anhnhatanhnhat1232@gmail.com>
2025-04-07 22:34:41 +0700

    Update Aoyama .lua

---
+++ Aoyama .lua
@@ -0,0 +1,14 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua"))()
+
+local Window = MakeWindow({
+  Hub = {
+    Title = "Aoyama x Nhat Anh",
+    Animation = "2001"
+  },
+  Key = {
+    KeySystem = true,
+    Title = "Key System",
+    Description = "Nhập key để tiếp tục sử dụng script.",
+    KeyLink = "https://your-key-page.com", -- Có thể để trống nếu không cần
+    Keys = {"Aoyama"},
+    Notifi = {
